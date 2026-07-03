cf0dee4fc49c9e626568f640308090f584cd4083
Yash <yashrajchandra9th13@gmail.com>
2026-07-03 12:47:01 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>
+       <h1>App</h1>
+       <p>This is a simple React app.</p>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
